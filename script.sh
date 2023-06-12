bf5d906625c50f1808a159c4eb3fc5fd27cc5524
Ramazan Zholdas <61927828+RamazanZholdas@users.noreply.github.com>
2023-06-12 18:13:31 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,55 @@
 #!/bin/bash
 output=$(aws secretsmanager get-secret-value --region us-east-1 --secret-id redisProdEndpoints --query SecretString --output text | jq -r .endpoints)
-password=$(aws secretsmanager get-secret-value --region us-east-1 --secret-id redisToken --query SecretString --output text | jq -r .redisAuthToken)
-IFS=',' read -ra endpoints <<< "$output"
+names=$(aws secretsmanager get-secret-value --region us-east-1 --secret-id redisProdEndpoints --query SecretString --output text | jq -r .names)
+passwords=$(aws secretsmanager get-secret-value --region us-east-1 --secret-id redisProdEndpoints --query SecretString --output text | jq -r .token)
+redisInsightPassword=$(aws secretsmanager get-secret-value --region us-east-1 --secret-id ramazansRedisInsightPassword --query SecretString --output text | jq -r .redisInsightPassword)
 
-for endpoint in "${endpoints[@]}"; do
+IFS=',' read -ra endpoints <<< "$output"
+IFS=',' read -ra namesArr <<< "$names"
+IFS=',' read -ra passwordsArr <<< "$passwords"
+
+for index in "${!endpoints[@]}"; do
+    endpoint="${endpoints[index]}"
+    name="${namesArr[index]:-}"
+    password="${passwordsArr[index]:-}"
+
+    if [ -z "$name" ]; then
+        name="$endpoint"
+    fi
+
+    if [ -z "$password" ]; then
+        password=""
+
+        if [ "$index" -eq "0" ]; then
+            password="$redisInsightPassword"
+        fi
+    fi
+
+    # Remove trailing comma if it exists in the last character of name, password, and endpoint
+    name="${name%,}"
+    password="${password%,}"
+    endpoint="${endpoint%,}"
+
     payload=$(cat <<EOF
 {
-    "name": "$endpoint",
+    "name": "$name",
     "connectionType": "STANDALONE",
     "password": "$password",
     "host": "$endpoint",
-    "port": 6379
+    "port": 6379,
+    "tls": {
+        "useTls": true,
+        "clientAuth": false,
+        "verifyServerCert": false
+    }
 }
 EOF
 )
 
     response=$(curl -X POST \
-        http://localhost:8001/api/instance/ \
+        https://redis.training.edetekapps.com/prod/api/instance/ \
         -H 'Content-Type: application/json' \
+        -H "password: $redisInsightPassword" \
         -d "$payload")
 
 done
